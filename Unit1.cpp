059ebc8312cc796d9aa1ed962b0c1f8e8ef883df
pepe <zlobnykokosik@gmail.com>
2021-01-16 14:31:41 +0300

    pep

---
+++ Unit1.cpp
@@ -0,0 +1,29 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	 Form1->Canvas->Brush->Color = clSilver;
+	Form1->Canvas-> Ellipse(0, 0, 10, 20);
+
+    Form1->Canvas->Brush->Color = clNavy;
+	Form1->Canvas->Ellipse(20, 20, 100, 100);
+}
+
+
+//---------------------------------------------------------------------------
+
+
